8fab3bb67d1624deed0ea729e5b6b20655cc3a66
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-12-13 18:09:06 +0300

    Added teacher solution for task: Вычислить факториал

---
+++ 1-module/1-task/index.js
@@ -0,0 +1,12 @@
+function factorial(n) {
+  let result = 1;
+  if (n === 0 || n === 1) {
+    return result;
+  }
+
+  for (let i = 1; i <= n; i++) {
+    result = result * i;
+  }
+
+  return result;
+}
