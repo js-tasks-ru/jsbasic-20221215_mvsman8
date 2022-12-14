8a64f1fa9692b4ff7d49b2a2b15ea135d39bbfda
v.mikolin <v.mikolin@sitesoft.ru>
2022-12-14 22:05:06 +0500

    uppercase first character

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,10 @@
 function ucFirst(str) {
   // ваш код...
+  if (!str) {
+    return "";
+  }
+
+  return str.length === 1
+    ? str.toUpperCase()
+    : `${str[0].toUpperCase()}${str.slice(1)}`;
 }
