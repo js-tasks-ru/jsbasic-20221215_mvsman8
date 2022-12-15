03d2062b9803b642a3a19c8019c69becdde5ca55
v.mikolin <v.mikolin@sitesoft.ru>
2022-12-15 21:27:41 +0500

    sum salary func

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -2,7 +2,7 @@ function sumSalary(salaries) {
   // ваш код...
   let sum = 0;
 
-  for (let key in salaries) {
+  for (const key in salaries) {
     const value = salaries[key];
 
     if (typeof value === "number" && isFinite(value)) {
