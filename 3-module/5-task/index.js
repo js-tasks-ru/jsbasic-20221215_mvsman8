b647fac49427a693fc02f37287fc9aa7786ed634
v.mikolin <v.mikolin@sitesoft.ru>
2022-12-25 14:49:52 +0500

    getMinMax

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
   // ваш код...
+  const numbers = str
+    .split(" ")
+    .filter((num) => !isNaN(num) && parseFloat(num));
+
+  return {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers),
+  };
 }
