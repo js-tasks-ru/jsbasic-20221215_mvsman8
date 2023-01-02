5b886296d52b00d2a10b89dde027dbbae9500ccf
v.mikolin <v.mikolin@sitesoft.ru>
2023-01-02 14:18:48 +0500

    makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,16 @@
 function makeFriendsList(friends) {
   // ваш код...
+  const body = document.body;
+  const ul = document.createElement("ul");
+
+  for (let i = 0; i < friends.length; i++) {
+    const { firstName, lastName } = friends[i];
+    const li = `<li>${firstName} ${lastName}</li>`;
+
+    ul.insertAdjacentHTML("beforeend", li);
+  }
+
+  body.appendChild(ul);
+
+  return ul;
 }
