2a79ca2855bd6dba6107388ac95e28937bf0e0e6
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-01-07 14:23:32 +0300

    Added task Учебный проект: Карусель

---
+++ 6-module/3-task/slides.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: 'Penang shrimp',
+    price: 16,
+    image: 'penang_shrimp.png',
+    id: 'penang-shrimp'
+  },
+  {
+    name: 'Chicken cashew',
+    price: 14,
+    image: 'chicken_cashew.png',
+    id: 'chicken-cashew'
+  },
+  {
+    name: 'Red curry veggies',
+    price: 12.5,
+    image: 'red_curry_vega.png',
+    id: 'red-curry-veggies'
+  },
+  {
+    name: 'Chicken springrolls',
+    price: 6.5,
+    image: 'chicken_loempias.png',
+    id: 'chicken-springrolls'
+  }
+];
