927f0f1132bf8e2764ba2442e2f58797ef066e0e
v.mikolin <v.mikolin@sitesoft.ru>
2023-01-11 12:52:11 +0500

    7.2 modal

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,77 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Modal {
   constructor() {
+    this.render();
+  }
+
+  render() {
+    this.elem = createElement(`
+      <div class="modal">
+        <div class="modal__overlay"></div>
+        <div class="modal__inner">
+          <div class="modal__header">
+            <button type="button" class="modal__close">
+              <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+            </button>
+            <h3 class="modal__title">
+            </h3>
+          </div>
+          <div class="modal__body">
+          </div>
+        </div>
+      </div>
+    `);
+  }
+
+  #el(el) {
+    return document.querySelector(`${el}`);
+  }
+
+  #modal(el) {
+    return this.elem.querySelector(`.modal__${el}`);
+  }
+
+  setTitle(string) {
+    this.#modal("title").textContent = string;
+  }
+
+  setBody(node) {
+    this.#modal("body").innerHTML = node.outerHTML;
+  }
+
+  open() {
+    this.handleOpen();
+
+    this.#modal("close").onclick = this.handleClose.bind(this);
+
+    const onKeydownCloseHandler = ({ code }) => {
+      if (code === "Escape") {
+        this.handleClose();
+      }
+    };
+
+    document.onkeydown = onKeydownCloseHandler;
+  }
+
+  handleOpen() {
+    this.#el("body").append(this.elem);
+    this.#el("body").classList.add("is-modal-open");
+  }
+
+  close() {
+    this.handleClose();
+  }
+
+  handleClose() {
+    this.elem.remove();
+    this.#el("body").classList.remove("is-modal-open");
+
+    this.removeEvents();
+  }
+
+  removeEvents() {
+    document.onkeydown = null;
+    this.#modal("close").onclick = null;
   }
 }
