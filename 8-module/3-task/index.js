9a064c8ebe9972135ac4c8c29ea717e680cadc70
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-01-14 15:57:09 +0300

    Added teacher solution for task: Учебный проект: Корзина, часть 1

---
+++ 8-module/3-task/index.js
@@ -0,0 +1,62 @@
+export default class Cart {
+  cartItems = []; // [product: {...}, count: N]
+
+  constructor(cartIcon) {
+    this.cartIcon = cartIcon;
+  }
+
+  addProduct(product) {
+    if (!product) {
+      return;
+    }
+
+    let cartItem = this.cartItems.find(
+      item => item.product.id === product.id
+    );
+
+    if (!cartItem) {
+      cartItem = {
+        product,
+        count: 1
+      };
+      this.cartItems.push(cartItem);
+    } else {
+      cartItem.count++;
+    }
+
+    this.onProductUpdate(cartItem);
+  }
+
+  updateProductCount(productId, amount) {
+    let cartItem = this.cartItems.find(item => item.product.id == productId);
+    cartItem.count += amount;
+
+    if (cartItem.count === 0) {
+      this.cartItems.splice(this.cartItems.indexOf(cartItem), 1);
+    }
+
+    this.onProductUpdate(cartItem);
+  }
+
+  onProductUpdate() {
+    // реализуем в следующей задаче
+
+    this.cartIcon.update(this);
+  }
+
+  isEmpty() {
+    return this.cartItems.length === 0;
+  }
+
+  getTotalCount() {
+    return this.cartItems.reduce((sum, item) => sum + item.count, 0);
+  }
+
+  getTotalPrice() {
+    return this.cartItems.reduce(
+      (sum, item) => sum + item.product.price * item.count,
+      0
+    );
+  }
+}
+
