440b7da09751e1a334a3f530906cf060ff16258a
v.mikolin <v.mikolin@sitesoft.ru>
2023-01-16 15:22:51 +0500

    9.1 promise click

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -1,3 +1,5 @@
 export default function promiseClick(button) {
-  // ваш код...
+  return new Promise((resolve, reject) =>
+    button.addEventListener("click", (e) => resolve(e), { once: true })
+  );
 }
